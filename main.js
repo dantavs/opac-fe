0a67c6f8e6bd5a6d55b14bc66be8280a72ff936f
Daniel Tavares <dantavs.gmail.com>
2023-10-15 12:35:29 +0000

    connect to bff

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ console.log(window.location.href)
 const devUrl = "localhost"
 const prodURL = ""
 
+const apiHost = "https://opac-9ey2.onrender.com"
+
 const currentUrl = window.location.hostname
 
 var urlText = "prod"
@@ -12,3 +14,18 @@ if (currentUrl == devUrl){
 }
 
 document.getElementById("url").innerHTML= urlText
+
+async function getGameInfo(){
+    const url = apiHost + "/getGameInfo"
+    const request = await fetch(url,{method:"GET"})
+
+    const gameInfo = await request.json()
+
+    return gameInfo
+}
+
+async function startGame() {
+    const apiReturn = await getGameInfo()
+
+    document.getElementById("gameInfo").innerHTML = apiReturn
+}
