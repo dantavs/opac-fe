dc66ae45a1ba0b1cd81f1dab0bce6940ad0798d0
Daniel Tavares <dantavs.gmail.com>
2023-10-17 16:10:40 +0000

    next round debug

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
-console.log(window.location.href)
-
 const devUrl = "localhost"
 const prodURL = ""
 
 var apiHost = "https://opac-9ey2.onrender.com"
+var envText = "prod"
 
 const currentUrl = window.location.hostname
 
-var urlText = "prod"
-
 if (currentUrl == devUrl){
-    urlText = "Dev"
+    envText = "Dev"
     apiHost = "http://localhost:3333"
 }
 
-document.getElementById("url").innerHTML= urlText
+document.getElementById("url").innerHTML= envText
 
 async function getGameInfo(){
     const url = apiHost + "/getGameInfo"
@@ -44,29 +41,75 @@ async function getGameData(){
     return gameInfo
 }
 
-const gameButton = document.getElementById("btAction")
 
-gameButton.onclick = async function () {
-    const gameData = await getGameData()
-    
+async function nextRound(gameData){
+    const url = apiHost + "/OPGNextRound"
+    const game = JSON.parse(gameData)
+
+    alert('vai chamar: ' + gameData)
+    const request = await fetch(url,{
+        method: "POST"
+        ,body: gameData
+    })
+
+    const updatedGameData = await request.json()
+
+    alert('status code: ', JSON.stringify(updatedGameData))
+
+    return updateGameData
+}
+
+function updatePlayersData(gameData){
     document.getElementById("playerAName").innerHTML = gameData.playerA.name
     document.getElementById("playerAHP").innerHTML = gameData.playerA.hp
-    //document.getElementById("playerACardName").innerHTML = gameData.playerACard.name
-    //document.getElementById("playerACardPower").innerHTML = gameData.playerACard.power
-    //document.getElementById("playerACardImg").innerHTML = gameData.playerACard.img
 
     document.getElementById("playerBName").innerHTML = gameData.playerB.name
     document.getElementById("playerBHP").innerHTML = gameData.playerB.hp
-    //document.getElementById("playerBCardName").innerHTML = gameData.playerBCard.name
-    //document.getElementById("playerBCardPower").innerHTML = gameData.playerBCard.power
-    //document.getElementById("playerBCardImg").innerHTML = gameData.playerBCard.img
-
-    //const winnerPlayer = gameData.winner == "playerA" ? gameData.playerACard.name + " (" + gameData.playerA.name +")" : gameData.playerBCard.name + " (" + gameData.playerB.name +")"  
-    //const winnerText = "Winner: " + winnerPlayer
-    //document.getElementById("winner").innerHTML = winnerPlayer
 
     document.getElementById("playersInfo").style.display = "block"
-    document.getElementById("roundInfo").style.display = "block"
 
-    document.getElementById("btAction").innerHTML="Next round"
+    return true
+}
+
+function updateGameData(game){
+    updatePlayersData(game)
+
+        document.getElementById("playerACardName").innerHTML = gameData.playerACard.name
+        document.getElementById("playerACardPower").innerHTML = gameData.playerACard.power
+        document.getElementById("playerACardImg").innerHTML = gameData.playerACard.img
+        
+        document.getElementById("playerBCardName").innerHTML = gameData.playerBCard.name
+        document.getElementById("playerBCardPower").innerHTML = gameData.playerBCard.power
+        document.getElementById("playerBCardImg").innerHTML = gameData.playerBCard.img
+        
+        const winnerPlayer = gameData.winner == "playerA" ? gameData.playerACard.name + " (" + gameData.playerA.name +")" : gameData.playerBCard.name + " (" + gameData.playerB.name +")"  
+        const winnerText = "Winner: " + winnerPlayer
+        document.getElementById("winner").innerHTML = winnerPlayer
+        
+        document.getElementById("roundInfo").style.display = "block"
+        
+        
+
+        return true
+}
+
+const gameButton = document.getElementById("btAction")
+
+gameButton.onclick = async function () {
+    const gameState = document.getElementById('gameState').innerHTML
+
+    if (gameState === "inProgress"){
+        const gameData = document.getElementById("gameData").innerHTML
+        const game = await nextRound(gameData)
+        const updatedGameData = updateGameData(game)
+        document.getElementById("gameData").innerHTML = updatedGameData
+    }else{   
+        const gameData = await getGameData()
+        updatePlayersData(gameData)
+        document.getElementById("gameData").innerHTML=JSON.stringify(gameData)
+        document.getElementById("gameState").innerHTML = "inProgress"
+        document.getElementById("btAction").innerHTML="Next round"
+    }
+
+    return true
 }
